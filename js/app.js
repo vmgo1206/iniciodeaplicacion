af095263122164148e08b18012c5e001b048b435
vmgo1206 <94582034+vmgo1206@users.noreply.github.com>
2022-03-10 23:53:16 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,49 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'md',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'RunGran Shop',
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/contenido/',
+    	url: 'contenido.html',
+    	name: 'contenido',
+  		},
+		{
+		path: '/offers/',
+    	url: 'offers.html',
+    	name: 'offers',
+  		}
+	],
+	dialog: {
+		title: 'RunGran Shop',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
